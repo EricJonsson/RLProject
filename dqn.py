b70ee7d4b46352251f69d5abf4a45baf6a6333ed
Joakim <joakim.michalak@gmail.com>
2021-05-18 12:00:04 +0200

    NN fix

--- dqn.py
+++ dqn.py
@@ -60,6 +60,11 @@ class DQN(nn.Module):
 
     def forward(self, x):
         """Runs the forward pass of the NN depending on architecture."""
+        x = self.relu(self.conv1(x))
+        x = self.relu(self.conv2(x))
+        x = self.relu(self.conv3(x))
+        x = x.view(x.size(0), -1)
+
         x = self.relu(self.fc1(x))
         x = self.fc2(x)
 
